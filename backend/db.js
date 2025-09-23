5ccedd61fe149be13bfd75ab13ae37ba5a701fd9
Azathulhu <65434073+Azathulhu@users.noreply.github.com>
2025-09-23 19:03:20 +0800

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,15 +3,13 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // required for Neon on Render
 });
 
 pool.on('error', (err) => {
   console.error('Unexpected PG error', err);
+  process.exit(-1);
 });
 
 module.exports = pool;
