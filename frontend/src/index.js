cf065d531869c16359a72c2f076970a134112828
Azathulhu <65434073+Azathulhu@users.noreply.github.com>
2025-09-26 10:59:43 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router basename="/">
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
