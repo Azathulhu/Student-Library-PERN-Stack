ac1302be9b2c59f0a3b0a8780a4f5a74a1206e39
Azathulhu <65434073+Azathulhu@users.noreply.github.com>
2025-09-26 08:41:09 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,10 @@ import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 );
